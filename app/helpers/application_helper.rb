4538caf4c58241da471ecdf7df4c8ffa36c59c45
ruuuh20 <pkim2320@gmail.com>
2017-10-24 15:56:33 -0400

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Peak Fall 2017 English"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
