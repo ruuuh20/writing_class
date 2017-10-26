47a0ce4548edeec66937c330af1b1b532a2615ff
ruuuh20 <pkim2320@gmail.com>
2017-10-26 11:45:22 -0400

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
 
   def new
+     @user = User.new
+   end
+
+   def create
+     @user = User.new(user_params)
+     if @user.save
+      #  @user.send_activation_email
+       flash[:info] = "Please check your email to activate your account."
+       redirect_to root_url
+     else
+       render 'new'
+     end
+   end
+
+  def show
+    @user = User.find(params[:id])
   end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 
 
