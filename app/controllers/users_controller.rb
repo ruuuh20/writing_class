d88b2caaa771b35c94566ac53fcad71a299d4eee
ruuuh20 <pkim2320@gmail.com>
2017-11-08 20:42:37 -0500

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
      @user = User.new(user_params)
      if @user.save
       #  @user.send_activation_email
-       log_in @user
-        flash[:success] = "Welcome to the Peak App"
-       redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
      else
        render 'new'
      end
